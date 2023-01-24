63ddf1379a761fccd30f7b9010c9b0d7f1cccb29
Ion Stici <ionsticidev@gmail.com>
2023-01-24 13:48:10 +0200

    Move data to a JS file (because of error when fetching data from a json file while the project is live)

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
+import { data } from "./data.js";
+const timeframes = data.map((obj, i) => obj.timeframes);
+
 const timeEls = document.querySelectorAll(".card__time");
 const prevTimeEls = document.querySelectorAll(".card__prev-time");
 const btns = document.querySelectorAll(".profile__btn");
 const btnsContainer = document.querySelector(".profile__buttons");
 const activeBtnClass = "profile__btn--active";
-let timeframes, data;
-
-fetch("./../../data.json")
-    .then((res) => res.json())
-    .then((data) => (timeframes = data.map((obj, i) => obj.timeframes)));
 
 const removeActiveBtn = () =>
     btns.forEach((btn) => btn.classList.remove(activeBtnClass));
@@ -15,14 +13,15 @@ const removeActiveBtn = () =>
 const renderData = function (btn, period) {
     removeActiveBtn();
     btn.classList.add(activeBtnClass);
+    let currData;
 
-    if (period === "Daily") data = timeframes.map((d) => d.daily);
-    if (period === "Weekly") data = timeframes.map((d) => d.weekly);
-    if (period === "Monthly") data = timeframes.map((d) => d.monthly);
+    if (period === "Daily") currData = timeframes.map((d) => d.daily);
+    if (period === "Weekly") currData = timeframes.map((d) => d.weekly);
+    if (period === "Monthly") currData = timeframes.map((d) => d.monthly);
 
-    timeEls.forEach((el, i) => (el.textContent = `${data[i].current}hrs`));
+    timeEls.forEach((el, i) => (el.textContent = `${currData[i].current}hrs`));
     prevTimeEls.forEach(
-        (el, i) => (el.textContent = `Last Day - ${data[i].previous}hrs`)
+        (el, i) => (el.textContent = `Last Day - ${currData[i].previous}hrs`)
     );
 };
 
